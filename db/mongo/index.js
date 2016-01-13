2ffed5f6ccb7545c381291bcc9ae484ac4b61ef1
wanglei <wanglei8381@163.com>
2016-01-13 23:27:50 +0800

    edit mongo

--- db/mongo/index.js
+++ db/mongo/index.js
@@ -9,12 +9,156 @@ var ReplSet = mongodb.ReplSet;
 var Mongos = mongodb.Mongos;
 var Db = mongodb.Db;
 var Collection = mongodb.Collection;
+var status = true;
 
 var mongo = module.exports = function (options) {
   config.options = _.deepExtend(config.options, options);
+  //mongodb.Server的实例
+  mongo.servers = [];
+  //mongodb.Db的实例
+  mongo.cache = {};
+  //临时缓存的数据库名称
+  mongo.stack = [];
   return mongo;
 };
 
+/**
+ * 创建数据库连接
+ * @param callback
+ */
 mongo.createConnection = function (callback) {
+  var _this = this;
+  callback = callbackHandler(callback, 'create a mongodb connection');
+  var options = config.options;
+  var database = config.database;
+  var type = config.type;
+  this.servers = config.hosts.map(function (ip) {
+    var vary = ip.split(':');
+    var server = new Server(vary[0], parseInt(vary[1]), options['server']);
+  });
+  var db = null;
+  switch (type) {
+    case 'standalone':
+      db = new Db(database, this.servers[0], options['db']);
+      mongo.openConnection(db, callback);
+      break;
+    case 'replset':
+      var replset = new ReplSet(this.servers, options['replSet']);
+      db = new Db(database, replset, options['db']);
+      mongo.openConnection(db, callback);
+      break;
+    case 'cluster':
+      var mongos = new Mongos(ServerArray, options['mongos']);
+      db = new Db(database, mongos, options['db']);
+      mongo.openConnection(db, callback);
+      break;
+    default:
+      var usernamePassword = '';
+      if (config.user && config.password) {
+        usernamePassword = config.user + ':' + encodeURIComponent(config.password) + '@';
+      }
+
+      var connString = 'mongodb://' + usernamePassword + config.hosts[0] + '/' + database;
+      MongoClient.connect(connString, options, function (err, _db) {
+        if (err) {
+          console.log(new Date().format("iso") + ' could not connect to your Mongo database. Mongo returned the following error : %s', err.message);
+          return callback(err, null);
+        }
+        _this.cache[database] = _db;
+        callback(null, _db);
+      });
+  }
 
 };
+
+//打开数据库连接
+mongo.openConnection = function (db, callback) {
+  callback = callbackHandler(callback, 'open a mongodb connection');
+  var _this = this;
+  var options = config.options;
+  var database = config.database;
+  db.open(function (err, _db) {
+    if (err) {
+      console.log(new Date().format("iso") + ' could not connect to your Mongo database. Mongo returned the following error : %s', err.message);
+    }
+    _db.on('error', function (error) {
+      console.log(new Date().format("iso") + ' could not connect to your Mongo database. Mongo returned the following error : %s', err.message);
+    });
+    _db.on('close', function () {
+      console.log(new Date().format("iso") + ' Mongo already colse !');
+    });
+    _db.on('fullsetup', function () {
+      // console.log(new Date().format("iso") + ' Mongo already connect to server ! ');
+    });
+    if (config.user && config.password) {
+      _db.authenticate(config.user, config.password, function (err, result) {
+        if (err || result === false) {
+          return callback(err, null);
+        } else {
+          _this.cache[database] = _db;
+          return callback(null, _db);
+        }
+      });
+    } else {
+      _this.cache[mongo_defaultDb] = _db;
+      callback(null, _db);
+    }
+  });
+};
+
+/**
+ * 获取一个数据库db实例,
+ * 首先会先从缓存中获取，
+ * 如果缓存中没有，则使用默认的数据库切换到指定的数据
+ * @param dbName
+ * @param callback
+ */
+mongo.getConnect = function (dbName, callback) {
+  callback = callbackHandler(callback, 'get a mongodb connection');
+  var _this = this;
+  var options = config.options;
+  var database = config.database;
+  var _db = this.cache[dbName];
+  if (_db != null && typeof _db === 'object') {
+    callback(null, _db);
+  } else {
+    var _db_default = this.cache[database];
+    if (_db_default != null && typeof _db_default === 'object') {
+      var _db_select = _db_default.db(dbName);
+      this.cache[dbName] = _db_select;
+      callback(null, _db_select);
+      return;
+    }
+    var stack = _this.stack;
+    stack.push({
+      cb: callback,
+      dbName: dbName
+    });
+    // 当有一个连接初始化请求时，挂起其他初始化请求
+    // 连接池建立完后，使用该连接处理挂起的请求
+    if (status) {
+      status = false;
+      mongo.createConnection(function (err, db) {
+        var _db_default = _this.cache[database] = db;
+        for (var i = 0, len = stack.length; i < len; i++) {
+          var temp = stack.shift();
+          var fn = temp.cb;
+          var dbName = temp.dbName;
+          var _db_select = _this.cache[dbName] = _db_default.db(dbName);
+          fn(null, _db_select);
+        }
+      });
+    }
+  }
+};
+
+function callbackHandler(callback, msg) {
+  if (typeof callback !== 'function') {
+    callback = function () {
+      if (msg) {
+        console.log(msg + ', did not set the callback function');
+      }
+    };
+  }
+  return callback;
+}
